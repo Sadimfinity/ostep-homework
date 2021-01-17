4171b7cfd8bb3f21cec2a6eaa54eb9c5c5dc5a3c
sadimfinity <santi-skate@hotmail.com>
2021-01-17 00:18:36 -0500

    Making the basic structure of the main interface

--- file-ffs/example.py
+++ file-ffs/example.py
@@ -1,12 +1,33 @@
-import tkinter as tk
-
-class MyApp():
-    def __init__(self):
-        self.root = tk.Tk()
-        l1 = tk.Label(self.root, text="Hello")
-        l2 = tk.Label(self.root, text="World")
-        l1.grid(row=0, column=0, padx=(100, 10))
-        l2.grid(row=1, column=0, padx=(10, 100)) 
-
-app = MyApp()
-app.root.mainloop()
+from tkinter import * 
+  
+root = Tk() 
+  
+# specify size of window. 
+root.geometry("250x170") 
+  
+# Create text widget and specify size. 
+T = Text(root, height = 5, width = 52) 
+  
+# Create label 
+l = Label(root, text = "Fact of the Day") 
+l.config(font =("Courier", 14)) 
+  
+Fact = """A man can be arrested in 
+Italy for wearing a skirt in public."""
+  
+# Create button for next text. 
+b1 = Button(root, text = "Next", ) 
+  
+# Create an Exit button. 
+b2 = Button(root, text = "Exit", 
+            command = root.destroy)  
+  
+l.pack() 
+T.pack() 
+b1.pack() 
+b2.pack() 
+  
+# Insert The Fact. 
+T.insert(END, Fact) 
+  
+mainloop() 
--- file-ffs/main.py
+++ file-ffs/main.py
@@ -6,10 +6,18 @@ class Interfaz:
         self.window = Tk()
         # Inicializar la ventana con un título
         self.window.title("File Fast System")
+        self.window.geometry("840x500") 
         self.createAndPositionButtons()
         self.createAndPositionEntries()
         self.createAndPositionLabels()
+
+        T = Text(self.window, height = 23, width = 50) 
+
+        T.grid(row=5, column=1, sticky=W, pady=(30,5), padx=4) 
+
         self.window.mainloop()
+
+
         return
 
     def createAndPositionButtons(self):
@@ -27,7 +35,8 @@ class Interfaz:
 
         for i, val in enumerate(buttons):
             val.grid(row=i + 1, column=0, sticky=W, pady=4, padx=4)
-
+        execFfs.grid(row=6, column=1, pady=4)
+        loadFile.grid(row=7, column=2, pady=4)
         return
 
     def createAndPositionEntries(self):
@@ -42,9 +51,11 @@ class Interfaz:
         action = Label(self.window, text = "Acción", font=('mincho', 11))
         name = Label(self.window, text = "Nombre", font=('mincho', 11)) 
         size = Label(self.window, text = "Tamaño", font=('mincho', 11))
+        infoLabel = Label(self.window, text = "En caso de tener el txt listo, puede cargar el archivo")
         labels = [action, name, size] 
         for i, val in enumerate(labels):
             val.grid(row=0, column=i, pady=4, padx=4)
+        infoLabel.grid(row=7, column=1, pady=4, padx=4)
         return
 
 
